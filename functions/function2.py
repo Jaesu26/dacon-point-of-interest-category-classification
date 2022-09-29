6b38d36b5d0ad51b7f28c9a320906206382d7f7d
Jaesu Han <84672164+Jaesu26@users.noreply.github.com>
2022-09-30 01:11:39 +0900

    Update function2.py

--- functions/function2.py
+++ functions/function2.py
@@ -3,7 +3,7 @@ def clean_texts(texts: Sequence[str]) -> NDArray:
     for text in texts: 
         text = text.lower()
         text = re.sub(r'\w*\d\w*', '', text)  ## 숫자 단어 제거
-        text = re.sub(r"[^\uAC00-\uD7A3\s\.]", ' ', text)  ## 한글 음절, 띄어쓰기, 마침표 제외하고 모두 삭제
+        text = re.sub(r'[^\uAC00-\uD7A3\s\.]', ' ', text)  ## 한글 음절, 띄어쓰기, 마침표 제외하고 모두 삭제
         text = re.sub(r'\s+', ' ', text)  ## extra space 제거
         text = text.strip()
         new_texts.append(text) 
